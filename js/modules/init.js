b4c2b516cd5ce406f8f149dc1469dd31255017ac
shreecheryl <shreecheryl@att.net>
2023-04-15 16:34:28 -0700

    final clean up

--- js/modules/init.js
+++ js/modules/init.js
@@ -2,7 +2,6 @@ async function fetchEmployees() {
     try {
         const response = await fetch('../data/employees.json');
         const employees = await response.json();
-        console.log(employees);
         return employees;
     }
     catch (error) {
